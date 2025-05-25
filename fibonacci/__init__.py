f66db61e9408a6b8650074c3f190780be097298a
gergelydani17 <74675380+gergelydani17@users.noreply.github.com>
2025-05-25 18:16:09 +0200

    Update __init__.py

--- fibonacci/__init__.py
+++ fibonacci/__init__.py
@@ -4,12 +4,12 @@ fszamok = [0, 1]
 
 i = 0
 k = 2
-#fibonacci számok feltöltése listába az input által megadott számig
+
 while i <= x:
     fszamok.append(fszamok[k-1] + fszamok[k-2])
     k += 1
     i = fszamok[k-1]
-#a 3-mal oszthatók kiválasztása és kiírása
+
 j = 0
 while j < len(fszamok):
     if fszamok[j] % 3 == 0:
